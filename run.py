584a29f9c64a66495d35615de69bb6fe6587263a
gayatrig19 <>
2023-10-06 13:56:22 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print('Welcome to THE Quest For The Lost Treasure')
+print ("""
+        Step 1: In the heart of the bustling coastal town of Port Haven, a whispered      
+                legend has captured the imaginations of adventurers and dreamers alike.   
+                It speaks of a long-lost treasure, hidden away by a legendary pirate      
+                captain, Captain Blackbeard, centuries ago. The treasure is said to be     
+                unimaginable, with jewels that glitter like stars and riches beyond       
+                measure.\n
+                You are Captain Alex Sterling, a seasoned sailor and fearless leader      
+                known for your daring voyages across treacherous seas. The lure of the     
+                lost treasure beckons to you, as it does to so many others. Assemble your  
+                crew, for you have resolved to embark on "The Quest for the Lost Treasure."\n
+                As you set sail aboard the "Black Serpent," your trusty ship, two paths  
+                stretch before you, each with its own challenges and mysteries.      
+""")
+print("""
+        Step 2: You've chosen to navigate the Enchanted Forest, a place where magic        
+                swirls through the air and mythical creatures roam. As you journey        
+                deeper into the forest, you encounter a wise old owl perched on a          
+                moss-covered branch. The owl speaks to you: 'The path to the treasure      
+                lies through these woods, but it is treacherous. You must choose your      
+                way wisely.'      
+""")
+print("""
+        Step 3: You've decided to brave the treacherous Mountains of Shadows, a place     
+                known for its rugged peaks and unforgiving terrain. As you ascend the     
+                steep slopes, you come across a hermit who lives among the peaks. The      
+                hermit offers guidance: 'The path to the treasure is hidden among these   
+                mountains, but it is not for the faint of heart. Choose your path wisely.'
+""")
+print("""
+       Step 4: You accept the owl's guidance and follow its directions deeper into the   
+               Enchanted Forest. The owl leads you to an ancient tree with inscriptions. 
+               It reveals a hidden path to the Treasure Cove. 
+""")
+print("""
+       Step 5: You decide to continue your journey without the owl's help. As you       
+               venture deeper into the Enchanted Forest, you encounter a mystical pond   
+               with shimmering waters. It reflects the image of a hidden path.           
+""")
+print("""
+       Step 6: You decide to trust the hermit's guidance and follow their directions.    
+              They lead you through treacherous mountain passes and reveal the          
+              entrance to the Treasure Cave.  
+""")
