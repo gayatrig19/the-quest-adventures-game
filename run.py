d3227c5cd032b8563a95789a0fdc89b4898e2755
gayatrig19 <>
2023-10-06 14:14:18 +0000

    Add the adventure game story with decision choices

--- run.py
+++ run.py
@@ -11,7 +11,14 @@ print ("""
                 lost treasure beckons to you, as it does to so many others. Assemble your  
                 crew, for you have resolved to embark on "The Quest for the Lost Treasure."\n
                 As you set sail aboard the "Black Serpent," your trusty ship, two paths  
-                stretch before you, each with its own challenges and mysteries.      
+                stretch before you, each with its own challenges and mysteries. 
+                --------------------------------------------------------------------------
+                Option 1: Do you navigate the Enchanted Forest, where magic fills the     
+                          air and mythical creatures roam?                                          
+                Option 2: Or do you brave the treacherous Mountains of Shadows, where     
+                          the secrets of the Scepter may be concealed among the rugged 
+                          peaks?
+                ---------------------------------------------------------------------------     
 """)
 print("""
         Step 2: You've chosen to navigate the Enchanted Forest, a place where magic        
@@ -19,7 +26,11 @@ print("""
                 deeper into the forest, you encounter a wise old owl perched on a          
                 moss-covered branch. The owl speaks to you: 'The path to the treasure      
                 lies through these woods, but it is treacherous. You must choose your      
-                way wisely.'      
+                way wisely.' 
+                ----------------------------------------------------------------------- 
+                Option 1: Do you follow the guidance of the wise owl?                     
+                Option 2: Or do you venture deeper into the forest without its help?  
+                -----------------------------------------------------------------------     
 """)
 print("""
         Step 3: You've decided to brave the treacherous Mountains of Shadows, a place     
@@ -27,19 +38,34 @@ print("""
                 steep slopes, you come across a hermit who lives among the peaks. The      
                 hermit offers guidance: 'The path to the treasure is hidden among these   
                 mountains, but it is not for the faint of heart. Choose your path wisely.'
+                --------------------------------------------------------------------------
+                Option 1: Do you trust the hermit's guidance and follow their directions? 
+                Option 2: Or do you continue your own way through the mountains? 
+                --------------------------------------------------------------------------
 """)
 print("""
        Step 4: You accept the owl's guidance and follow its directions deeper into the   
                Enchanted Forest. The owl leads you to an ancient tree with inscriptions. 
                It reveals a hidden path to the Treasure Cove. 
+               ------------------------------------------------------------------------
+               Option 1: Do you follow the hidden path to the Treasure Cove?             
+               Option 2: Or do you continue your own way through the forest?  
+               ------------------------------------------------------------------------
 """)
 print("""
        Step 5: You decide to continue your journey without the owl's help. As you       
-               venture deeper into the Enchanted Forest, you encounter a mystical pond   
-               with shimmering waters. It reflects the image of a hidden path.           
+               venture deeper into the Enchanted Forest, you encounter a mystical 
+               pond with shimmering waters. It reflects the image of a hidden path.
+               ------------------------------------------------------------------------
+               Option 1: Do you follow the path reflected in the pond's waters?         
+               Option 2: Or do you choose to explore a different route?
+               ------------------------------------------------------------------------              
 """)
 print("""
        Step 6: You decide to trust the hermit's guidance and follow their directions.    
               They lead you through treacherous mountain passes and reveal the          
               entrance to the Treasure Cave.  
+              -------------------------------------------------------------------------
+              Option 1: Do you enter the Treasure Cave?                                 
+              Option 2: Or do you continue exploring the mountains on your own?  
 """)
