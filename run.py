6008721b4a46c63ebcf266bb311e62d1312814b1
gayatrig19 <>
2023-10-07 18:44:42 +0000

    Create display_intro function to display game introduction

--- run.py
+++ run.py
@@ -1,71 +1,33 @@
-print('Welcome to THE Quest For The Lost Treasure')
-print ("""
-        Step 1: In the heart of the bustling coastal town of Port Haven, a whispered      
-                legend has captured the imaginations of adventurers and dreamers alike.   
-                It speaks of a long-lost treasure, hidden away by a legendary pirate      
-                captain, Captain Blackbeard, centuries ago. The treasure is said to be     
-                unimaginable, with jewels that glitter like stars and riches beyond       
-                measure.\n
-                You are Captain Alex Sterling, a seasoned sailor and fearless leader      
-                known for your daring voyages across treacherous seas. The lure of the     
-                lost treasure beckons to you, as it does to so many others. Assemble your  
-                crew, for you have resolved to embark on "The Quest for the Lost Treasure."\n
-                As you set sail aboard the "Black Serpent," your trusty ship, two paths  
-                stretch before you, each with its own challenges and mysteries. 
-                --------------------------------------------------------------------------
-                Option 1: Do you navigate the Enchanted Forest, where magic fills the     
-                          air and mythical creatures roam?                                          
-                Option 2: Or do you brave the treacherous Mountains of Shadows, where     
-                          the secrets of the Scepter may be concealed among the rugged 
-                          peaks?
-                ---------------------------------------------------------------------------     
-""")
-print("""
-        Step 2: You've chosen to navigate the Enchanted Forest, a place where magic        
-                swirls through the air and mythical creatures roam. As you journey        
-                deeper into the forest, you encounter a wise old owl perched on a          
-                moss-covered branch. The owl speaks to you: 'The path to the treasure      
-                lies through these woods, but it is treacherous. You must choose your      
-                way wisely.' 
-                ----------------------------------------------------------------------- 
-                Option 1: Do you follow the guidance of the wise owl?                     
-                Option 2: Or do you venture deeper into the forest without its help?  
-                -----------------------------------------------------------------------     
-""")
-print("""
-        Step 3: You've decided to brave the treacherous Mountains of Shadows, a place     
-                known for its rugged peaks and unforgiving terrain. As you ascend the     
-                steep slopes, you come across a hermit who lives among the peaks. The      
-                hermit offers guidance: 'The path to the treasure is hidden among these   
-                mountains, but it is not for the faint of heart. Choose your path wisely.'
-                --------------------------------------------------------------------------
-                Option 1: Do you trust the hermit's guidance and follow their directions? 
-                Option 2: Or do you continue your own way through the mountains? 
-                --------------------------------------------------------------------------
-""")
-print("""
-       Step 4: You accept the owl's guidance and follow its directions deeper into the   
-               Enchanted Forest. The owl leads you to an ancient tree with inscriptions. 
-               It reveals a hidden path to the Treasure Cove. 
-               ------------------------------------------------------------------------
-               Option 1: Do you follow the hidden path to the Treasure Cove?             
-               Option 2: Or do you continue your own way through the forest?  
-               ------------------------------------------------------------------------
-""")
-print("""
-       Step 5: You decide to continue your journey without the owl's help. As you       
-               venture deeper into the Enchanted Forest, you encounter a mystical 
-               pond with shimmering waters. It reflects the image of a hidden path.
-               ------------------------------------------------------------------------
-               Option 1: Do you follow the path reflected in the pond's waters?         
-               Option 2: Or do you choose to explore a different route?
-               ------------------------------------------------------------------------              
-""")
-print("""
-       Step 6: You decide to trust the hermit's guidance and follow their directions.    
-              They lead you through treacherous mountain passes and reveal the          
-              entrance to the Treasure Cave.  
-              -------------------------------------------------------------------------
-              Option 1: Do you enter the Treasure Cave?                                 
-              Option 2: Or do you continue exploring the mountains on your own?  
-""")
+class TreasureQuest:
+    """
+    TreasureQuest class
+    """
+    def __init__(self):
+        # Constructor logic here
+        pass  # Placeholder for constructor logic
+
+    def display_intro(self):
+        """
+        Display the game's introduction.
+        """
+        print ("""
+               --------------------------------------------------------------------------
+                  Welcome to "The Quest for the Lost Treasure" - an adventure that will
+                  take you on a thrilling journey into the heart of mystery and legend.
+               --------------------------------------------------------------------------
+        """)
+
+# Create an instance of the TreasureQuest class
+game = TreasureQuest()
+
+# Call the display_intro method
+game.display_intro()
+
+
+
+
+
+
+
+
+       
