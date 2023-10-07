8357e49b28e71afc4cc92f56c3cee952b38c0242
gayatrig19 <>
2023-10-07 19:10:31 +0000

    Add rules for the game to display_intro function

--- run.py
+++ run.py
@@ -16,6 +16,21 @@ class TreasureQuest:
                   take you on a thrilling journey into the heart of mystery and legend.
                --------------------------------------------------------------------------
         """)
+        print("""
+                 Your goal is to find the legendary treasure hidden by Captain Blackbeard.
+                 Choose your path wisely to succeed. At each step, you will be presented 
+                 with options - Option 1 and Option 2. Enter the number of your choice.
+                 
+                 Explore different paths, meet mystical creatures, and make decisions to shape 
+                 your destiny.
+                 
+                 Your adventure will lead to various outcomes, including success and disappointment.
+                 
+                 Reach the end and discover the treasure to win! Or face a different fate based on 
+                 your choices. Enjoy the journey and embrace the excitement of the unknown. 
+                                  
+                                  Good luck, Captain!
+        """)
 
 # Create an instance of the TreasureQuest class
 game = TreasureQuest()
